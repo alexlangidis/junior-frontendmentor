104d05cd8fed44af7bf38e9b5995a60ff6dd063e
alexlag7 <alexlag.dev@gmail.com>
2023-10-05 22:32:02 +0300

    main

---
+++ intro-section-with-dropdown-navigation/app/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    
+    
+    
+    $('.dropdown').click(function (e) { 
+        e.preventDefault();
+        $(this).find('#arrow').toggleClass('rotate-180 duration-300');
+        toggleSubMenu(this);
+    });
+    
+    function toggleSubMenu(dropdown) {
+        $(dropdown).find('.sub-menu').toggleClass('hidden');
+    }
+    
+
+    $('.dropdown-mobile').click(function (e) { 
+        e.preventDefault();
+        $(this).find('#arrow').toggleClass('rotate-180 duration-300');
+        toggleSubMenu(this);
+    });
+    
+    function toggleSubMenu(dropdown) {
+        $(dropdown).find('.sub-menu-mobile').toggleClass('hidden');
+    }
+
+    // $('.open').click(function (e) { 
+    //     e.preventDefault();
+    //     $('.mobile-menu').toggleClass('hidden flex');
+    //     $('.overflow').toggleClass('hidden flex');
+    // });
+
+    // $('.close').click(function (e) { 
+    //     e.preventDefault();
+    //     $('.mobile-menu').toggleClass('hidden flex');
+    //     $('.overflow').toggleClass('hidden flex');
+    // });
+
+    function toggleMenu() {
+        $('.mobile-menu, .overflow').toggleClass('hidden flex animate__fadeInDown');
+    }
+    
+    $('.open, .close').click(function (e) {
+        e.preventDefault();
+        toggleMenu();
+    });
+    
+});
